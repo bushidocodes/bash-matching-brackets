8c1f4a96eeabcd63bee9d79b6454a0a962cba3e3
Sean McBride <spmcbride1201@gmail.com>
2020-01-11 17:28:04 -0500

    feat: complete solution

--- matching_brackets.sh
+++ matching_brackets.sh
@@ -1,48 +1,48 @@
 #!/usr/bin/env bash
 
-# Good reference: https://linuxhint.com/associative_array_bash/
-
 # shellcheck disable=SC1091
 source stack.sh
 
-declare last_bracket
+# shellcheck disable=SC2034
+stack=()
 
-process_brace() {
-  local brace=$1
-  case $brace in
-    "[")
-      push "["
-      return 0
+declare last_bracket
+declare -A matching_brackets=( 
+  ["]"]="[" 
+  ["}"]="{"
+  [")"]="("
+)
+
+process_bracket() {
+  local bracket=$1
+  case $bracket in
+    "[" | "{" | "(" )
+      push stack "$bracket"
       ;;
-    "]")
-      local last_bracket
-      if [[ $last_bracket -eq "[" ]]; then
-        return 0
-      else
-        return 1
-      fi
+    "]" | "}" | ")" )
+      pop stack last_bracket
+      [ "$?" -eq 1 ] && return 1
+      [[ $last_bracket == "${matching_brackets[$bracket]}" ]]
       ;;
   esac
 }
 
 main() {
-  local braces=$1
+  local brackets=$1
   local -a char_arr
 
-  for (( i=0 ; i < ${#braces} ; i++ )); do 
-    char_arr[i]=${braces:i:1}; 
+  for (( i=0; i < ${#brackets}; i++ )); do 
+    char_arr[i]=${brackets:i:1}; 
   done
 
-  echo "$char_arr[@]"
-
-  push 5
-  print
-  push 10
-  print
-  local popped_val=99
-  pop "popped_val"
-  printf "Pop: %d\n" "$popped_val"
-  print
+  for char in "${char_arr[@]}"; do
+    if ! process_bracket "$char"; then
+      printf "false\n"
+      return 0
+    fi
+  done
+  
+  is_empty stack && echo true || echo false
 }
 
 main "$@"
